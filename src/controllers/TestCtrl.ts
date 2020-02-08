d199a494bd02d6f3cd56720a69c8a1c399b35552
romain.lenzotti <rom.lenzotti@gmail.com>
2020-02-08 10:55:48 +0100

    chore: Update project and add locus

--- src/controllers/TestCtrl.ts
+++ src/controllers/TestCtrl.ts
@@ -1,61 +1,53 @@
-import { Controller, Get, PathParams, $log, Post, BodyParams, Put, QueryParams, Delete } from '@tsed/common';
-import { Returns } from '@tsed/swagger';
-import { Data } from '../models/Data';
+import {BodyParams, Controller, Delete, Get, PathParams, Post, Put, QueryParams} from "@tsed/common";
+import {Returns} from "@tsed/swagger";
+import {Data} from "../models/Data";
 
-@Controller('ctrl')
+@Controller("ctrl")
 export class TestCtrl {
 
-	@Get('/:id')
-	@Returns(Data)
-	async get(
-		@PathParams('id') id: number,
-	): Promise<Data> {
-		$log.info(`GET - id ${id}`);
+  @Get("/:id")
+  @Returns(Data)
+  async get(
+    @PathParams("id") id: number
+  ): Promise<Data> {
+    return {
+      id,
+      name: `${id} name`
+    };
+  }
 
-		return {
-			id,
-			name: `${id} name`
-		};
-	}
+  @Post("/:id")
+  @Returns(Data)
+  async post(
+    @PathParams("id") id: number,
+    @BodyParams("name") name: string
+  ): Promise<Data> {
+    return {
+      id,
+      name
+    };
+  }
 
-	@Post('/:id')
-	@Returns(Data)
-	async post (
-		@PathParams('id') id: number,
-		@BodyParams('name') name: string
-	): Promise<Data> {
-		$log.info(`POST - id ${id} `)
+  @Put("/:id")
+  @Returns(Data)
+  async put(
+    @PathParams("id") id: number,
+    @QueryParams("name") name: string
+  ): Promise<Data> {
+    return {
+      id,
+      name
+    };
+  }
 
-		return {
-			id,
-			name
-		};
-	}
-
-	@Put('/:id')
-	@Returns(Data)
-	async put (
-		@PathParams('id') id: number,
-		@QueryParams('name') name: string
-	): Promise<Data> {
-		$log.info(`PUT - id ${id} `)
-
-		return {
-			id,
-			name
-		};
-	}
-
-	@Delete('/:id')
-	@Returns(Data)
-	async delete(
-		@PathParams('id') id: number,
-	): Promise<Data> {
-		$log.info(`DELETE - id ${id} `)
-
-		return {
-			id,
-			name: `${id} name`
-		};
-	}
+  @Delete("/:id")
+  @Returns(Data)
+  async delete(
+    @PathParams("id") id: number
+  ): Promise<Data> {
+    return {
+      id,
+      name: `${id} name`
+    };
+  }
 }
